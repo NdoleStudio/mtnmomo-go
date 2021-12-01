384b47deccbe72a77147b17a8e1f0dd8f370b6f2
Acho Arnold <arnold@ndolestudio.com>
2021-12-01 19:32:03 +0200

    Fix reason to pay

--- collection.go
+++ collection.go
@@ -12,25 +12,19 @@ type RequestToPayParams struct {
 
 // RequestToPayStatus is the status of a request to pay request.
 type RequestToPayStatus struct {
-	Amount                 string              `json:"amount"`
-	Currency               string              `json:"currency"`
-	ExternalID             string              `json:"externalId"`
-	Payer                  *RequestToPayPayer  `json:"payer"`
-	Status                 string              `json:"status"`
-	FinancialTransactionID *string             `json:"financialTransactionId,omitempty"`
-	Reason                 *RequestToPayReason `json:"reason,omitempty"`
+	Amount                 string             `json:"amount"`
+	Currency               string             `json:"currency"`
+	ExternalID             string             `json:"externalId"`
+	Payer                  *RequestToPayPayer `json:"payer"`
+	Status                 string             `json:"status"`
+	FinancialTransactionID *string            `json:"financialTransactionId,omitempty"`
+	Reason                 *string            `json:"reason,omitempty"`
 }
 
 // RequestToPayPayer identifies an account holder in the wallet platform.
 type RequestToPayPayer struct {
 	PartyIDType string `json:"partyIdType"`
 	PartyID     string `json:"partyId"`
-}
-
-// RequestToPayReason contains the cause in case of failure.
-type RequestToPayReason struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
 }
 
 // AccountBalance is available balance of the account
